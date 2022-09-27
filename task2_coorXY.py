1dc5981d4fc98effff5ff33f168559a5801ac27d
Rafail Abdyushev <rafaelab@mail.ru>
2022-09-27 15:24:35 +0300

    Task 2 check quater

---
+++ task2_coorXY.py
@@ -0,0 +1,46 @@
+class ProgramXY:
+    def __init__(this):
+        this.strLegend = ["Введите пожалуйста координату для X: ", "Введите пожалуйста координату для Y: "]
+        this.counterInput = 0
+        this.coorXY = []
+
+        this.InitClear()
+
+    def InitClear(this):
+        import os
+        clear = lambda: os.system("CLS")
+        clear()
+
+    def InputFunc(this):
+        while this.counterInput < 2:
+            this.inputStr = input(this.strLegend[this.counterInput])
+            # print(this.inputStr.isdigit())
+            if this.inputStr.lstrip("-").isdigit() and this.inputStr != "0":
+                this.coorXY.append(int(this.inputStr))
+                this.counterInput += 1
+                this.InputFunc()
+            else:
+                print("Вы ввели не соответсвующую запись! Попробуйте еще раз")
+                this.InputFunc()
+        return
+
+    def CheckQuater(this):
+        check = this.coorXY[0] * this.coorXY[1]
+        if check > 0:
+            if this.coorXY[0] > 0:
+                print(f"Ваши координаты x: {this.coorXY[0]}, y: {this.coorXY[1]} находятся в первой четверти")
+            else:
+                print(f"Ваши координаты x: {this.coorXY[0]}, y: {this.coorXY[1]} находятся в третьей четверти")
+        else:
+            if this.coorXY[0] > 0:
+                print(f"Ваши координаты x: {this.coorXY[0]}, y: {this.coorXY[1]} находятся в четвертой четверти")
+            else:
+                print(f"Ваши координаты x: {this.coorXY[0]}, y: {this.coorXY[1]} находятся во второй четверти")
+
+
+
+#   исполнение программы
+
+exec = ProgramXY()
+exec.InputFunc()
+exec.CheckQuater()
