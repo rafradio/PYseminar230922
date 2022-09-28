7b3dcbbc68e1cff6280e47a8fe0e1a81bada72d0
Rafail Abdyushev <rafaelab@mail.ru>
2022-09-28 14:55:00 +0300

    check expression

---
+++ task1_1checkExpr.py
@@ -0,0 +1,8 @@
+import os
+clear = lambda: os.system("CLS")
+clear()
+
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(f"({x, y, z}): {(not(x or y or z)) == (not x and not y and not z)}")
